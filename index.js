ecbbb70f4e4e882ef03b619368077413679ecc52
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-10 11:11:30 +0100

    so?

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+/*
+ Aufgabenstellung - Teil 1
+
+*/
+const zahlen = [2011, 2003, 2020, 1998, 2019, 2022, 2003];
+
+zahlen.forEach(
+    (einzelneZahl) => {
+        console.log("Zahlen: ", einzelneZahl);
+    }
+);
+let einzelneZahl = zahlen;
+console.log(einzelneZahl);
