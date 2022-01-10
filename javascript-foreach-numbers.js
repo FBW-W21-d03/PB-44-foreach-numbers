45d8db18144007dbfdf708b7b08d58e8ec0dbb34
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-10 11:08:08 +0100

    Orhan's lösung

---
+++ javascript-foreach-numbers.js
@@ -0,0 +1,13 @@
+console.log("Aufgabe 1: ");
+let arrayJahre = [2011, 2003, 2020, 1998, 2019, 2022, 2003];
+console.log("\n", arrayJahre);
+
+
+
+console.log("Aufgabe 2: \n");
+arrayJahre.forEach(dasJahr => {
+
+    console.log("Das Jahr: ", dasJahr)
+
+});
+
