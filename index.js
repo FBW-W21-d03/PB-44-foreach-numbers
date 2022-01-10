b5d35cf667541c0edf30347f7ad3131996adabc0
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-10 12:12:07 +0100

    MeineLösung

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Aufgabe 1: ");
+let arrayZahl = [2011, 2003, 2020, 1998, 2019, 2022, 2003];
+console.log("", arrayZahl);
+
+
+
+console.log("Aufgabe 2: ");
+arrayZahl.forEach(dasJahr => {
+
+    console.log("arrayZahl", dasJahr)
+
+});
